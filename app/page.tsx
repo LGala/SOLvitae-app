17c1bc225082a2304aded37de8589a727854e604
LGala <galafassilorenzo@gmail.com>
2024-07-06 16:31:28 +0200

    push form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  NumberInput,
+  TextInput,
+} from "@mantine/core";
+import { useState } from "react";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  const [isFirstPage, setIsFirstPage] = useState(0);
+  const [isSecondPage, setIsSecondPage] = useState(0);
+
+  return (
+    <Container
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <form onSubmit={() => {}}>
+        <TextInput label="Name" placeholder="Name" />
+        <TextInput mt="sm" label="Email" placeholder="Email" />
+        <NumberInput mt="sm" label="Age" placeholder="Age" min={0} max={99} />
+        <Container
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button type="submit" mt="sm">
+            Submit
+          </Button>
+        </Container>
+      </form>
+    </Container>
+  );
 }

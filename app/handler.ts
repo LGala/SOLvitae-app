b0785946a0f45bb3138dcfde31e0c7b45b4fa01a
LGala <galafassilorenzo@gmail.com>
2024-07-13 16:08:33 +0200

    add nft creation

--- app/handler.ts
+++ app/handler.ts
@@ -1,10 +1,8 @@
 import { ResumeData } from "./page";
 import { createMintAccountWithMetadata } from "./scripts/create-mint-account-with-metadata";
-import { createTokenAccountAndMintNFT } from "./scripts/create-token-account-and-mint-NFT";
 
 export async function createResume(): Promise<void> {
-  // to simulate an API call
-  return new Promise(resolve => setTimeout(resolve, 1 * 1000));
+  // create new airdropped wallet 
 }
 
 export async function uploadResume(resumeData: ResumeData): Promise<void> {
@@ -12,10 +10,7 @@ export async function uploadResume(resumeData: ResumeData): Promise<void> {
 
   console.log("Creation of Mint Account with metadata...");
   await createMintAccountWithMetadata(resumeData.companyName, resumeData.contractType, resumeData.roleStartedDate, resumeData.roleEndedDate, resumeData.workModel, resumeData.jobPosition, resumeData.jobPositionDescription);
-  
-  console.log("Creation of Token Account and minting NFT...")
-  await createTokenAccountAndMintNFT();
-  
+
   // to simulate an API call
   return new Promise(resolve => setTimeout(resolve, 1 * 1000));
 }
--- app/scripts/create-mint-account-with-metadata.ts
+++ app/scripts/create-mint-account-with-metadata.ts
@@ -5,7 +5,9 @@ import {
     TYPE_SIZE,
     createInitializeMetadataPointerInstruction,
     createInitializeMintInstruction,
-    getMintLen
+    createMintToInstruction,
+    getMintLen,
+    getOrCreateAssociatedTokenAccount
 } from "@solana/spl-token";
 import {
     TokenMetadata,
@@ -21,27 +23,25 @@ import {
     clusterApiUrl,
     sendAndConfirmTransaction
 } from "@solana/web3.js";
-import fs from "fs";
 import payerWalletSecretKey from "../../wallets/payer-secret-key.json";
 
 
 export async function createMintAccountWithMetadata(companyName: string, contractType: string, roleStart: string, roleEnd: string, workModel: string, jobPosition: string, jobPositionDescription: string) {
     const payer = Keypair.fromSecretKey(new Uint8Array(payerWalletSecretKey));
-    
+
     // Connection to devnet cluster
     const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    
+
     // Transaction to send
     let transaction: Transaction;
     // Transaction signature returned from sent transaction
     let transactionSignature: string;
-    
-    
+
+
     //********* Mint Account Setup *********//
-    
+
     // Generate new keypair for Mint Account
     const mintKeypair = Keypair.generate();
-    fs.writeFileSync("wallets/mint-secret-key.json", JSON.stringify([...mintKeypair.secretKey]));
     // Address for Mint Account
     const mint = mintKeypair.publicKey;
     // Decimals for Mint Account
@@ -50,7 +50,7 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
     const mintAuthority = payer.publicKey;
     // Authority that can update the metadata pointer and token metadata
     const updateAuthority = payer.publicKey;
-        
+
     // Metadata to store in Mint Account
     const metaData: TokenMetadata = {
         updateAuthority: updateAuthority,
@@ -59,33 +59,33 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         symbol: "SOLVITAE",
         uri: "https://github.com/riomrz/SOLvitae-api/blob/main/assets/metadata.json",
         additionalMetadata: [
-            ["description", "dNFT representing the person's CV"], 
-            ["company name", companyName], 
+            ["description", "dNFT representing the person's CV"],
+            ["company name", companyName],
             ["contract type", contractType],
             ["role started", roleStart],
             ["role ended", roleEnd],
             ["work model", workModel],
-            ["job position", jobPosition],   
-            ["job position description", jobPositionDescription],   
+            ["job position", jobPosition],
+            ["job position description", jobPositionDescription],
         ],
     };
-    
+
     // Size of MetadataExtension 2 bytes for type, 2 bytes for length
     const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
     // Size of metadata
     const metadataLen = pack(metaData).length;
-    
+
     // Size of Mint Account with extension
     const mintLen = getMintLen([ExtensionType.MetadataPointer]);
-    
+
     // Minimum lamports required for Mint Account
     const lamports = await connection.getMinimumBalanceForRentExemption(
         mintLen + metadataExtension + metadataLen,
     );
-    
-    
+
+
     //********* Build Instructions *********//
-    
+
     // Instruction to invoke System Program to create new account
     const createAccountInstruction = SystemProgram.createAccount({
         fromPubkey: payer.publicKey, // Account that will transfer lamports to created account
@@ -94,16 +94,16 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         lamports, // Amount of lamports transferred to created account
         programId: TOKEN_2022_PROGRAM_ID, // Program assigned as owner of created account
     });
-    
+
     // Instruction to initialize the MetadataPointer Extension
     const initializeMetadataPointerInstruction =
-    createInitializeMetadataPointerInstruction(
-        mint, // Mint Account address
-        updateAuthority, // Authority that can set the metadata address
-        mint, // Account address that holds the metadata
-        TOKEN_2022_PROGRAM_ID,
-    );
-    
+        createInitializeMetadataPointerInstruction(
+            mint, // Mint Account address
+            updateAuthority, // Authority that can set the metadata address
+            mint, // Account address that holds the metadata
+            TOKEN_2022_PROGRAM_ID,
+        );
+
     // Instruction to initialize Mint Account data
     const initializeMintInstruction = createInitializeMintInstruction(
         mint, // Mint Account Address
@@ -112,7 +112,7 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         mint, // Optional Freeze Authority
         TOKEN_2022_PROGRAM_ID, // Token Extension Program ID
     );
-    
+
     // Instruction to initialize Metadata Account data
     const initializeMetadataInstruction = createInitializeInstruction({
         programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
@@ -124,7 +124,7 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         symbol: metaData.symbol,
         uri: metaData.uri,
     });
-    
+
     // Instruction to update metadata, adding custom fields
     const updateDescriptionInstruction = createUpdateFieldInstruction({
         programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
@@ -182,10 +182,10 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         field: metaData.additionalMetadata[7][0], // key
         value: metaData.additionalMetadata[7][1], // value
     });
-    
-    
+
+
     //********* Send Transaction *********//
-    
+
     // Add instructions to new transaction
     transaction = new Transaction().add(
         createAccountInstruction,
@@ -202,18 +202,64 @@ export async function createMintAccountWithMetadata(companyName: string, contrac
         updateJobPositionInstruction,
         updateJobPositionDescriptionInstruction
     );
-    
+
     // Send transaction
     transactionSignature = await sendAndConfirmTransaction(
         connection,
         transaction,
         [payer, mintKeypair], // Signers
     );
-    
+
     console.log(
         "\nCreate Mint Account:",
         `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`,
     );
-    // Last test, SOLVITAE mint account:
-    // Create Mint Account: https://solana.fm/tx/Rk1oZgP74msxTfwLra1ZtMXS6aMYT5mjrgmaXzgX5tuhNdSmqn49zEkE1quv23Ed81G5hqBkjKHb9rTpBbDeXgq?cluster=devnet-solana
+
+    // Create new Token Account, defaults to ATA
+    const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        payer, // payer
+        mint, // mint address
+        payer.publicKey, // token account owner
+        undefined,
+        undefined,
+        undefined,
+        TOKEN_2022_PROGRAM_ID // SPL Token program account (needed to add metadata)
+    );
+    // Token account already created: GmFhtc4qBvJVxTXNP45SJEGPoUyzcFcJRp2Ru4ZzUWh9
+
+    // Instruction to mint a single unit of the token
+    const initializeMintToInstruction = createMintToInstruction(
+        mint, // mint address
+        tokenAccount.address, // destination
+        mintAuthority, // mint authority
+        1, // amount: 1 for a NFT
+        [],
+        TOKEN_2022_PROGRAM_ID
+    );
+
+    // Create and send transaction
+    transaction = new Transaction().add(
+        initializeMintToInstruction,
+        //initializeSetAuthorityInstruction
+    )
+    transactionSignature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer], // Signers
+    );
+
+    console.log(
+        "\nTransaction Signature:",
+        `https://solana.fm/tx/${transactionSignature}?cluster=devnet`
+    );
+    // Last test, SOLVITAE tx signature:
+    // Transaction Signature: https://solana.fm/tx/418HMK9oNavdpnQP8W8QfwqCrofyXE5nWXamP9mQSy8BmXXrxXkUk863HvokCpkMnJ6XRzLoEBNCoHpNvYsrKkGs?cluster=devnet
+
+    console.log(
+        "\nToken Account: ",
+        `https://solana.fm/address/${tokenAccount.address}?cluster=devnet`
+    );
+    // Last test, SOLVITAE Token Account:
+    // Token Account:  https://solana.fm/address/ER6krr9s3h9zt33bAchR3mQXry31cZVQwsnXfDYHu64w?cluster=devnet
 }
